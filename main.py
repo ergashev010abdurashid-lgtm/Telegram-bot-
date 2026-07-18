e879f35c14294afe2afeb6aecf3b629ba7358d7c
ergashev010abdurashid-lgtm <ergashev010abdurashid@gmail.com>
2026-07-18 19:49:31 +0500

    Implement Telegram bot with start command

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Telegram bot ishga tushdi!")
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+TOKEN = 8981400107:AAG1ZKhhgeHdVzDI6OQVXcO8-Wu_tO_NBYI
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Salom! Bot ishlayapti ✅")
+
+app = Application.builder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
