14fccf8bb3d2b724fc4985823a861de05ffa49a8
ergashev010abdurashid-lgtm <ergashev010abdurashid@gmail.com>
2026-07-18 19:51:42 +0500

    Fix token formatting in main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = 8981400107:AAG1ZKhhgeHdVzDI6OQVXcO8-Wu_tO_NBYI
+TOKEN = "8981400107:AAG1ZKhhgeHdVzDI6OQVXcO8-Wu_tO_NBYI"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Salom! Bot ishlayapti ✅")
